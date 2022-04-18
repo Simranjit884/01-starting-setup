0739176d1d8a067ae01cf04dcc5b7bc50b714c94
samy884 <ssgill884@gmail.com>
2022-04-18 20:25:40 +0530

    adding form

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -10,7 +10,17 @@ const ExpenseForm = () => {
           <label>Title</label>
           <input type="text" />
         </div>
+        <div className="new-expense__control">
+          <label>Amount</label>
+          <input type="number" min="0.01" step="0.01"/>
+        </div>
+        <div className="new-expense__control">
+          <label>Date</label>
+          <input type="Date" min="2019-01-01" max="2022-12-31"/>
+        </div>
       </div>
     </form>
   );
 };
+
+export default ExpenseForm;
