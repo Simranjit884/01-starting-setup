01b60eb37eec4cf736a524f56732d74e392cdc59
samy884 <ssgill884@gmail.com>
2022-04-23 22:02:49 +0530

    using alternative conditioning

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -14,22 +14,26 @@ const Expenses = (props) => {
     return expense.date.getFullYear().toString()===selectedYear;
   });
 
+  let expensesContent = <p>No expense found.</p>;
+
+  if(filteredExpenses.length>0){
+    expensesContent = filteredExpenses.map((expenseItem) => 
+    <ExpenseItem
+      key={expenseItem.id}
+      title={expenseItem.title}
+      amount={expenseItem.amount}
+      date={expenseItem.date}
+    />
+    )
+  }
+
   return (
     <Card className="expenses">
       <ExpenseFilter
         selected={selectedYear}
         onFilteredYear={filteredYearHandler}
       />
-      {filteredExpenses.length === 0 && <p>No expense found.</p>}
-      {filteredExpenses.length > 0 &&
-        filteredExpenses.map((expenseItem) => 
-        <ExpenseItem
-          key={expenseItem.id}
-          title={expenseItem.title}
-          amount={expenseItem.amount}
-          date={expenseItem.date}
-        />
-        )}
+      {expensesContent}
     </Card>
   );
 };
