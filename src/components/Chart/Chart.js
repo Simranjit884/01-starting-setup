f88463be0c66522b31770b94d4e88a882176dd1a
samy884 <ssgill884@gmail.com>
2022-04-26 18:04:45 +0530

    calculating maximum value

--- src/components/Chart/Chart.js
+++ src/components/Chart/Chart.js
@@ -1,13 +1,15 @@
 import React from "react";
 
 const Chart = (props) => {
+    const dataPointValues = props.dataPoints.map(dataPoint => dataPoint.value);
+    const totalMaximum = Math.max(...dataPointVlues);
   return (
     <div className="chart">
       {props.dataPoints.map((dataPoint) => (
         <ChartBar
           key={dataPoint.label}
           value={dataPoint.value}
-          maxValue={null}
+          maxValue={totalMaximum}
           label={dataPoint.label}
         />
       ))}
